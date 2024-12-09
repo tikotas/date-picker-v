ab76e73f7ddf83d1d812bb2411ba55abd86e9667
tigran <tigran.sukiasyan@altacode.com>
2024-12-09 04:34:15 +0400

    fix: add type export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import DatePickerV from "./components/DatePickerV";
 import {useConfiguration} from "./hooks/useConfiguration";
-import {ColorOptions, DatePickerPropsType} from "./types"
+import {ColorOptions, DatePickerPropsType, DateType} from "./types"
 
 export default DatePickerV
-export type {DatePickerPropsType, ColorOptions, useConfiguration}
+export type {DatePickerPropsType, ColorOptions, useConfiguration, DateType}
